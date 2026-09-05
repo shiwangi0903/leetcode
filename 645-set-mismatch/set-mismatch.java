674d8a3e0c5cc79d521d3a7641d57ce0e4e453c5
shiwangi0903 <73519989+shiwangi0903@users.noreply.github.com>
2026-09-05 22:52:28 +0530

    Time: 3 ms (69.29%) | Memory: 47.4 MB (87.28%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+    int i=0;
+        while(i<nums.length)
+        {
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct])
+            {
+                swap(nums,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=j+1)
+                return new int[]{nums[j],j+1};
+        }
+        
+        return new int[] {-1,-1};
+    }
+    
+    void swap(int[] nums,int f,int s){
+        
+        int temp=nums[f];
+        nums[f]=nums[s];
+        nums[s]=temp;
+    }
+}
