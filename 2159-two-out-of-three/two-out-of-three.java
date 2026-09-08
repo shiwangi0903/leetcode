2625932987c4b5fa5c44bec43854455779b38eeb
shiwangi0903 <73519989+shiwangi0903@users.noreply.github.com>
2026-09-08 23:07:03 +0530

    Time: 6 ms (53.75%) | Memory: 47 MB (37.35%) - LeetSync

---
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        Set<Integer> set3 = new HashSet<>();
+
+        for (int n : nums1)
+            set1.add(n);
+
+        for (int n : nums2)
+            set2.add(n);
+
+        for (int n : nums3)
+            set3.add(n);
+
+        Set<Integer> result = new HashSet<>();
+
+        for (int n : set1) {
+            if (set2.contains(n) || set3.contains(n))
+                result.add(n);
+        }
+
+        for (int n : set2) {
+            if (set3.contains(n))
+                result.add(n);
+        }
+
+        return new ArrayList<>(result);
+    }
+}
